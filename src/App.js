46aa2118c1d444a92f470989eeae048c1a082005
zzachjoness <zzachjoness@gmail.com>
2021-08-26 10:56:13 -0400

    react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import NavigationBar from "./Components/NavigationBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Brands from "./Pages/brands";
+import Home from "./Pages/home";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
 	return (
-		<div className="App">
-			<NavigationBar />
-			<Brands />
-		</div>
+		<BrowserRouter>
+			<div className="App">
+				<NavigationBar />
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/brands" exact component={Brands} />
+				</Switch>
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,7 +1,9 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import { Container } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
 import "../Style/navbar.css";
 //import NavDropdown from "react-bootstrap/NavDropdown";
 
@@ -9,24 +11,25 @@ const NavigationBar = () => {
 	const Navi = () => (
 		<Navbar bg="light" expand="lg">
 			<Container>
-				<Navbar.Brand href="#home" id="navbar-brand">
-					SuperCarz
-				</Navbar.Brand>
+				<LinkContainer to="/">
+					<Navbar.Brand id="navbar-brand">SuperCarz</Navbar.Brand>
+				</LinkContainer>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto" id="navbar-links">
-						<Nav.Link href="#gasoline" id="navbar-link">
-							Gasoline
-						</Nav.Link>
-						<Nav.Link href="#electric" id="navbar-link">
-							Electric
-						</Nav.Link>
-						<Nav.Link href="#hybrid" id="navbar-link">
-							Hybrid
-						</Nav.Link>
-						<Nav.Link href="#brands" id="navbar-link">
-							Brandz
-						</Nav.Link>
+						<LinkContainer to="/gasoline">
+							<Nav.Link id="navbar-link">Gasoline</Nav.Link>
+						</LinkContainer>
+						<LinkContainer to="/electric">
+							<Nav.Link id="navbar-link">Electric</Nav.Link>
+						</LinkContainer>
+						<LinkContainer to="/hybrid">
+							<Nav.Link id="navbar-link">Hybrid</Nav.Link>
+						</LinkContainer>
+
+						<LinkContainer to="/brands">
+							<Nav.Link id="navbar-link">Brandz</Nav.Link>
+						</LinkContainer>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
