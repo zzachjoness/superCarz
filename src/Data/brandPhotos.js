b17b66e11cf3076f288b6b8eb087278e7c4963d7
zzachjoness <zzachjoness@gmail.com>
2021-08-31 09:58:58 -0400

    brand photos

--- src/Data/brandPhotos.js
+++ src/Data/brandPhotos.js
@@ -4,11 +4,54 @@ import Audi from "../Images/brands/Audi.png";
 import Bently from "../Images/brands/bently.png";
 import BMW from "../Images/brands/bmw.png";
 import Bugatti from "../Images/brands/bugatti.png";
+import Ferrari from "../Images/brands/ferrari.png";
+import Ford from "../Images/brands/ford.png";
+import Honda from "../Images/brands/honda.png";
+import Jaguar from "../Images/brands/jaguar.png";
+import Koenigsegg from "../Images/brands/koenigsegg.png";
+import Lamborghini from "../Images/brands/lamborghini.png";
+import Lancia from "../Images/brands/lancia.png";
+import Lexus from "../Images/brands/lexus.png";
+import Lotus from "../Images/brands/lotus.png";
+import Maserati from "../Images/brands/maserati.png";
+import MB from "../Images/brands/mb.png";
+import Mclaren from "../Images/brands/mclaren.png";
+import Nissan from "../Images/brands/nissan.png";
+import Pagani from "../Images/brands/pagani.png";
+import Porsche from "../Images/brands/porsche.png";
+import Renault from "../Images/brands/renault.png";
+import RR from "../Images/brands/RR.png";
+import Tesla from "../Images/brands/tesla.png";
+import Toyota from "../Images/brands/toyota.png";
 
 const brandsWithPhotos = [
 	{ name: "Acura", image: acura },
 	{ name: "Aston Martin", image: AM },
-	{ name: "BWM", image: BMW },
+	{ name: "Audi", image: Audi },
+	{ name: "Bently", image: Bently },
+	{ name: "BMW", image: BMW },
+	{ name: "Bugatti", image: Bugatti },
+	{ name: "Ferrari", image: Ferrari },
+	{ name: "Ford", image: Ford },
+	{ name: "Honda", image: Honda },
+	{ name: "Jaguar", image: Jaguar },
+	{ name: "Koenigsegg", image: Koenigsegg },
+	{ name: "Lamborghini", image: Lamborghini },
+	{ name: "Lancia", image: Lancia },
+	{ name: "Lexus", image: Lexus },
+	{ name: "Lotus", image: Lotus },
+	{ name: "Maserati", image: Maserati },
+	{ name: "Mclaren", image: Mclaren },
+	{ name: "Merceds Benz", image: MB },
+	{ name: "Mclaren", image: Mclaren },
+	{ name: "Mclaren", image: Mclaren },
+	{ name: "Nissan", image: Nissan },
+	{ name: "Pagani", image: Pagani },
+	{ name: "Porsche", image: Porsche },
+	{ name: "Renautl", image: Renault },
+	{ name: "Rolls-Royce", image: RR },
+	{ name: "Tesla", image: Tesla },
+	{ name: "Toyota", image: Toyota },
 ];
 
 export default brandsWithPhotos;
