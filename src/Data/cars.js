658ed8ba859b3859eb5d8bf6fb2d86a571f830d3
zzachjoness <zzachjoness@gmail.com>
2021-09-24 11:14:48 -0400

    cars data

--- src/Data/cars.js
+++ src/Data/cars.js
@@ -1,25 +1,75 @@
-const cars = [
-	{
+/*
+{
 		id: "",
 		brand: "",
 		model: "",
-		country: "",
+		built: "",
 		technical: {
-			horsepower: "",
-			torque: "",
+			horsepower: "", //hp
+			torque: "", //lb-ft
 			zeroToSixty: "",
-			topSpeed: "",
+			topSpeed: "", //mph
 			driveType: "",
 			engineLayout: "",
 			fuelType: "",
 			engine: "",
 			transmission: "",
 			mpgCombined: "",
-			weight: "",
+			weight: "", //lb
 			bodyStyle: "",
 		},
 		year: "",
 		class: "",
+		cost: ",
+	},
+*/
+
+const cars = [
+	{
+		id: "0",
+		brand: "Acura",
+		model: "NSX Type S",
+		built: "Ohio, USA",
+		technical: {
+			horsepower: "600",
+			torque: "492",
+			zeroToSixty: "2.5",
+			topSpeed: "191",
+			driveType: "AWD",
+			layout: "Longitudinally mid-mounted",
+			fuelType: "hybrid",
+			engine: "Twin-turbocharged DOHC V6 + Electric Motor Units",
+			transmission: "9-Speed DCT",
+			mpgCombined: "TBD",
+			weight: "3946",
+			bodyStyle: "coupe",
+		},
+		year: "2022",
+		class: "hybrid",
+		cost: "$172,000",
+	},
+	{
+		id: "1",
+		brand: "",
+		model: "",
+		built: "",
+		technical: {
+			horsepower: "", //hp
+			torque: "", //lb-ft
+			zeroToSixty: "",
+			topSpeed: "", //mph
+			driveType: "",
+			engineLayout: "",
+			fuelType: "",
+			engine: "",
+			transmission: "",
+			mpgCombined: "",
+			weight: "", //lb
+			bodyStyle: "",
+		},
+		year: "",
+		class: "",
+		cost: ",
 	},
 ];
 
