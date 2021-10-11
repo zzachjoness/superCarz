561db9f205627fd7fe21abe62629bb7371522cb9
zzachjoness <zzachjoness@gmail.com>
2021-10-11 16:32:15 -0400

    all cars style

--- src/Pages/allCars.js
+++ src/Pages/allCars.js
@@ -8,10 +8,7 @@ const AllCars = () => {
 
 	const SelectSort = (props) => {
 		return (
-			<div
-				id="all-cars-grid-sort-container"
-				onLoad={console.log("props: ", props)}
-			>
+			<div id="all-cars-grid-sort-container">
 				<div
 					id="all-cars-grid-sort-button"
 					onClick={() => {
@@ -33,18 +30,20 @@ const AllCars = () => {
 	};
 	const CarGrid = () =>
 		carData.map((car) => (
-			<div
-				id="all-cars-grid-car"
-				key={car.id}
-				onLoad={console.log("loaded: ", car.model)}
-			>
-				<div id="all-cars-grid-car-data">{car.brand}</div>
-				<div id="all-cars-grid-car-data">{car.model}</div>
-				<div id="all-cars-grid-car-data">{car.class}</div>
-				<div id="all-cars-grid-car-data">{car.year}</div>
-				<div id="all-cars-grid-car-data">{car.technical.horsepower}</div>
-				<div id="all-cars-grid-car-data">{car.technical.torque}</div>
-				<div id="all-cars-grid-car-data">{car.technical.zeroToSixty}</div>
+			<div id="all-cars-grid-car" key={car.id}>
+				<div id="all-cars-grid-car-data-brand">{car.brand}</div>
+				<div id="all-cars-grid-car-data-model">{car.model}</div>
+				<div id="all-cars-grid-car-data-class">{car.class}</div>
+				<div id="all-cars-grid-car-data-year">{car.year}</div>
+				<div id="all-cars-grid-car-data-technical-hp">
+					{car.technical.horsepower}
+				</div>
+				<div id="all-cars-grid-car-data-technical-torque">
+					{car.technical.torque}
+				</div>
+				<div id="all-cars-grid-car-data-technical-zeroSixty">
+					{car.technical.zeroToSixty}
+				</div>
 			</div>
 		));
 
@@ -56,31 +55,31 @@ const AllCars = () => {
 			<div id="all-cars-grid-body">
 				<div id="all-cars-grid-container">
 					<div id="all-cars-grid-header-container">
-						<div id="all-cars-grid-header-component">
+						<div id="all-cars-grid-header-component-brand">
 							<div id="all-cars-grid-header">brand</div>
 							<SelectSort name={["brand"]} reverse={false} />
 						</div>
-						<div id="all-cars-grid-header-component">
+						<div id="all-cars-grid-header-component-model">
 							<div id="all-cars-grid-header">model</div>
 							<SelectSort name={["model"]} reverse={false} />
 						</div>
-						<div id="all-cars-grid-header-component">
+						<div id="all-cars-grid-header-component-class">
 							<div id="all-cars-grid-header">class</div>
 							<SelectSort name={["class"]} reverse={false} />
 						</div>
-						<div id="all-cars-grid-header-component">
+						<div id="all-cars-grid-header-component-year">
 							<div id="all-cars-grid-header">year</div>
 							<SelectSort name={["year"]} reverse={true} />
 						</div>
-						<div id="all-cars-grid-header-component">
-							<div id="all-cars-grid-header">horsepower</div>
+						<div id="all-cars-grid-header-component-technical-hp">
+							<div id="all-cars-grid-header">hp</div>
 							<SelectSort name={["technical", "horsepower"]} reverse={true} />
 						</div>
-						<div id="all-cars-grid-header-component">
-							<div id="all-cars-grid-header">torque ftlb</div>
+						<div id="all-cars-grid-header-component-technical-torque">
+							<div id="all-cars-grid-header">t (ftlb)</div>
 							<SelectSort name={["technical", "torque"]} reverse={true} />
 						</div>
-						<div id="all-cars-grid-header-component">
+						<div id="all-cars-grid-header-component-technical-zeroSixty">
 							<div id="all-cars-grid-header">zero-sixty</div>
 							<SelectSort name={["technical", "zeroToSixty"]} reverse={false} />
 						</div>
