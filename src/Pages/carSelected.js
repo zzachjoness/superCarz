4fba016fee786856f8761d730535510bcbc3a19d
zzachjoness <zzachjoness@gmail.com>
2021-11-10 15:27:33 -0500

    carSelected Technical Data col 1 add

--- src/Pages/carSelected.js
+++ src/Pages/carSelected.js
@@ -18,6 +18,24 @@ const CarSelected = () => {
 		history.goBack();
 	};
 
+	/*
+technical: {
+			horsepower: 252, //hp
+			torque: 223, //lb-ft
+			zeroToSixty: 6.3,
+			topSpeed: 152, //mph
+			driveType: "AWD",
+			engineLayout: "Front-engine",
+			fuelType: "Gasoline",
+			engine: "2.0L Turbo-charged I4",
+			aspiration: "Turbo-charged",
+			transmission: "5-speed manual",
+			mpgCombined: 23,
+			weight: 3064, //lb
+			bodyStyle: "2-door coupe",
+		},
+	*/
+
 	return (
 		<div>
 			{!car ? (
@@ -36,7 +54,7 @@ const CarSelected = () => {
 					</div>
 					<div id="car-selected-container">
 						<div id="car-selected-data-container">
-							<h2 id="car-selected-data-title">{car.model}</h2>
+							<h1 id="car-selected-data-title">{car.model}</h1>
 							<div id="car-selected-data-container-link">
 								<h3 id="car-selected-data">Brand: </h3>
 								<LinkContainer to={`/brands/${car.brand}`}>
@@ -46,6 +64,16 @@ const CarSelected = () => {
 							<h3 id="car-selected-data">Year: {car.year}</h3>
 							<h3 id="car-selected-data">Manufactured: {car.built}</h3>
 							<h3 id="car-selected-data">Sale Price: ${carCost(car)} USD</h3>
+						</div>
+						<div id="car-selected-tech-container">
+							<div id="car-selected-tech-header">Technical Data</div>
+							<div id="car-selected-tech-data">Engine: {car.technical.engine}</div>
+							<div id="car-selected-tech-data">
+								Layout: {car.technical.engineLayout}
+							</div>
+							<div id="car-selected-tech-data">Drive: {car.technical.driveType}</div>
+							<div id="car-selected-tech-data">Fuel: {car.technical.fuelType}</div>
+							<div id="car-selected-tech-data">Body: {car.technical.bodyStyle}</div>
 						</div>
 					</div>
 				</div>
