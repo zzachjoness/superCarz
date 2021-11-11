d4e4500cb469d680a9e53172e08c5826bd31d738
zzachjoness <zzachjoness@gmail.com>
2021-11-11 16:11:45 -0500

    carSelected column skeleton

--- src/Pages/carSelected.js
+++ src/Pages/carSelected.js
@@ -53,45 +53,53 @@ technical: {
 						</div>
 					</div>
 					<div id="car-selected-container">
-						<div id="car-selected-data-container">
-							<div id="car-selected-data-title">{car.model}</div>
-							<div id="car-selected-data-container-link">
-								<div id="car-selected-data">Brand:</div>
-								<LinkContainer to={`/brands/${car.brand}`}>
-									<div id="car-selected-data-link">{car.brand}</div>
-								</LinkContainer>
+						<div id="car-selected-row">
+							<div id="car-selected-data-container">
+								<div id="car-selected-data-title">{car.model}</div>
+								<div id="car-selected-data-container-link">
+									<div id="car-selected-data">Brand:</div>
+									<LinkContainer to={`/brands/${car.brand}`}>
+										<div id="car-selected-data-link">{car.brand}</div>
+									</LinkContainer>
+								</div>
+								<div id="car-selected-data">Year: {car.year}</div>
+								<div id="car-selected-data">Manufactured: {car.built}</div>
+								<div id="car-selected-data">Sale Price: ${carCost(car)} USD</div>
 							</div>
-							<div id="car-selected-data">Year: {car.year}</div>
-							<div id="car-selected-data">Manufactured: {car.built}</div>
-							<div id="car-selected-data">Sale Price: ${carCost(car)} USD</div>
+							<div id="car-selected-tech-container">
+								<div id="car-selected-tech-header">Technical Data</div>
+								<div id="car-selected-tech-data">Engine: {car.technical.engine}</div>
+								<div id="car-selected-tech-data">
+									Layout: {car.technical.engineLayout}
+								</div>
+								<div id="car-selected-tech-data">Drive: {car.technical.driveType}</div>
+								<div id="car-selected-tech-data">Fuel: {car.technical.fuelType}</div>
+								<div id="car-selected-tech-data">Body: {car.technical.bodyStyle}</div>
+								<div id="car-selected-tech-data">
+									Horse Power: {car.technical.horsepower}
+								</div>
+								<div id="car-selected-tech-data">
+									Torque: {car.technical.torque} ft&#8901;lb
+								</div>
+								<div id="car-selected-tech-data">
+									Zero to Sixty: {car.technical.zeroToSixty} secs.
+								</div>
+								<div id="car-selected-tech-data">
+									Top Speed: {car.technical.topSpeed} mph
+								</div>
+								<div id="car-selected-tech-data">
+									Fuel Efficiency: {car.technical.mpgCombined} mpg combined
+								</div>
+								<div id="car-selected-tech-data">
+									Curb Weight: {car.technical.weight} lb
+								</div>
+							</div>
 						</div>
-						<div id="car-selected-tech-container">
-							<div id="car-selected-tech-header">Technical Data</div>
-							<div id="car-selected-tech-data">Engine: {car.technical.engine}</div>
-							<div id="car-selected-tech-data">
-								Layout: {car.technical.engineLayout}
-							</div>
-							<div id="car-selected-tech-data">Drive: {car.technical.driveType}</div>
-							<div id="car-selected-tech-data">Fuel: {car.technical.fuelType}</div>
-							<div id="car-selected-tech-data">Body: {car.technical.bodyStyle}</div>
-							<div id="car-selected-tech-data">
-								Horse Power: {car.technical.horsepower}
-							</div>
-							<div id="car-selected-tech-data">
-								Torque: {car.technical.torque} ft&#8901;lb
-							</div>
-							<div id="car-selected-tech-data">
-								Zero to Sixty: {car.technical.zeroToSixty} secs.
-							</div>
-							<div id="car-selected-tech-data">
-								Top Speed: {car.technical.topSpeed} mph
-							</div>
-							<div id="car-selected-tech-data">
-								Fuel Efficiency: {car.technical.mpgCombined} mpg combined
-							</div>
-							<div id="car-selected-tech-data">
-								Curb Weight: {car.technical.weight} lb
-							</div>
+						<div id="car-selected-row">
+							<div>hi</div>
+							<div>car picture</div>
+							<div>car about</div>
+							<div>other cars from this brand</div>
 						</div>
 					</div>
 				</div>
