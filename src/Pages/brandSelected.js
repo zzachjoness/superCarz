61c6f7e910ad7d41a96288902a6bfceee3e8cca1
zzachjoness <zzachjoness@gmail.com>
2022-01-04 23:06:43 -0500

    brandSelected style update

--- src/Pages/brandSelected.js
+++ src/Pages/brandSelected.js
@@ -17,10 +17,66 @@ const BrandSelected = (props) => {
 				({ name }) => name === capitalizeFirstLetter(selectedBrand)
 		  );
 	const findCars = cars.filter((car) => car.brand === brand.name);
-	const brandCars = findCars.map((car) => <div key={car.id}>{car.model}</div>);
 	const historyClick = () => {
 		history.goBack();
 	};
+
+	const brandAbout = (
+		<div id="brand-selected-data-container">
+			<h2 id="brand-selected-data-title">{brand.name}</h2>
+			<h3 id="brand-selected-data">Founded: {brand.founded}</h3>
+			<h3 id="brand-selected-data">Headquarters: {brand.headquarters}</h3>
+			<h3 id="brand-selected-data">{`Organization: ${brand.type} ${
+				brand.parent ? ` of ${brand.parent}` : ""
+			}`}</h3>
+
+			<h3 id="brand-selected-data">
+				Webiste:
+				<a
+					id="brand-selected-data-link"
+					href={brand.website}
+					target="_blank"
+					rel="noreferrer"
+					referrerPolicy="no-referrer"
+				>
+					{brand.name}
+				</a>
+			</h3>
+		</div>
+	);
+	/* test
+	function BrandsAboutTwo() {
+		return (
+			<div id="brand-selected-data-container">
+				<h2 id="brand-selected-data-title">{brand.name}</h2>
+				<h3 id="brand-selected-data">Founded: {brand.founded}</h3>
+				<h3 id="brand-selected-data">Headquarters: {brand.headquarters}</h3>
+				<h3 id="brand-selected-data">{`Organization: ${brand.type} ${
+					brand.parent ? ` of ${brand.parent}` : ""
+				}`}</h3>
+
+				<h3 id="brand-selected-data">
+					Webiste:
+					<a
+						id="brand-selected-data-link"
+						href={brand.website}
+						target="_blank"
+						rel="noreferrer"
+						referrerPolicy="no-referrer"
+					>
+						{brand.name}
+					</a>
+				</h3>
+			</div>
+		);
+	}
+	*/
+
+	const brandCars = findCars.map((car) => <div key={car.id}>{car.model}</div>);
+
+	const brandImage = (
+		<img id="brand-selected-image" src={brand.image} alt={brand.name}></img>
+	);
 
 	return (
 		<div>
@@ -39,32 +95,11 @@ const BrandSelected = (props) => {
 						</div>
 					</div>
 					<div id="brand-selected-container">
-						<div id="brand-selected-data-container">
-							<h2 id="brand-selected-data-title">{brand.name}</h2>
-							<h3 id="brand-selected-data">Founded: {brand.founded}</h3>
-							<h3 id="brand-selected-data">Headquarters: {brand.headquarters}</h3>
-							<h3 id="brand-selected-data">{`Organization: ${brand.type} ${
-								brand.parent ? ` of ${brand.parent}` : ""
-							}`}</h3>
-
-							<h3 id="brand-selected-data">
-								Webiste:
-								<a
-									id="brand-selected-data-link"
-									href={brand.website}
-									target="_blank"
-									rel="noreferrer"
-									referrerPolicy="no-referrer"
-								>
-									{brand.name}
-								</a>
-							</h3>
-						</div>
-						<img id="brand-selected-image" src={brand.image} alt={brand.name}></img>
+						{brandAbout}
+						{brandCars}
+						{brandImage}
 					</div>
 					<div id="brand-selected-about">{brand.about}</div>
-					<div>cars</div>
-					<div>{brandCars}</div>
 				</div>
 			)}
 		</div>
